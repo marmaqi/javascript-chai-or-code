4b31cdae5c0064d4d574cce99ae383f9d4f4d628
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-03-24 15:25:46 +0000

    datatype edit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";       // treat all JS code as newer version
+
+// alert(3+3) // we are using node, not browser
+
+
+let name = "hitesh";
+let age = 19;
+let isLoggedIn = true;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null
+// undefined
+// symbol => used for unique
+// object 
+
+console.log(typeof null);   // object
+console.log(typeof undefined);  //undefined
