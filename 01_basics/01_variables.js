433494d3b0c763d1ede82d6459f1cffdd40665f6
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-03-24 14:40:38 +0000

    practice complete

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -8,14 +8,21 @@ accountEmail = "ab@ab.com";
 accountName = "hitesh";
 accountCity = "Kharmang";
 
-/*
-    don't use var never because of block scoped
-*/
+// /*
+//     don't use var never because of block scoped
+// */
 
 
-// console.log(accountEmail);
-// console.log(accountName);
-// console.log(accountCity);
-// console.log(accountId);
+ // console.log(accountEmail);
+ // console.log(accountName);
+ // console.log(accountCity);
+ // console.log(accountId);
 
 console.table([accountId,accountEmail,accountName,accountCity])
+
+// let name = "Muhammad Ali";
+// let age = 29;
+// const contact = 923405699822;
+
+// console.log("Your Name is:",name, "and your Age is:",age);
+// console.log("Anyone can contact you on",contact,"number");
