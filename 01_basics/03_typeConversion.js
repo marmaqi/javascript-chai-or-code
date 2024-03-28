c4b644a479796fae2b71b5a65815282f9fa9ec27
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-03-28 06:36:07 +0000

    type conversion of data types

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,20 @@
+let score = "ali"
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//  33 => convert into number we get => 33
+//  "33ab" => NaN
+//  true => 1
+
+let isLoggedIn = "ali";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+//  "" => false
+//  "ali" => true
