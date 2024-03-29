dd5f78868d8aeaf09325443bd54188a59fc5acf7
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-03-29 19:55:34 +0000

    conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2" == 2);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+// strict check means (===)
+// console.log("2" ===2);
