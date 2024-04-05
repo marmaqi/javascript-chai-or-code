b5a621d3e902c3c02a196139534c40c9472b25fb
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-05 16:43:51 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+let anotherName = new String('Muhammad Ali');
+
+console.log(anotherName.charAt(2));
+console.log(anotherName.indexOf('a'));
+console.log(anotherName);
+
+console.log(anotherName.concat(' and I am 29 years old.'));
