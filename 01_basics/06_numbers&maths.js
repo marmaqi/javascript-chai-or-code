9c9fd7b15f33584b2929aae82bd785fb9d6cdc29
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-11 15:52:30 +0000

    numbers & mathematics

---
+++ 01_basics/06_numbers&maths.js
@@ -0,0 +1,29 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(1000);
+// console.log(balance.toFixed(1));
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+// const anotherNumber = 23.0988;
+// console.log(anotherNumber.toPrecision(3));
+
+// const anotherNumber = 236.0988;
+// console.log(anotherNumber.toPrecision(3));
+
+// const anotherNumber = 1123.0988;
+// console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.99));
+// console.log(Math.floor(4.99));
+// console.log(Math.ceil(4.99));
+console.log(Math.random());
+console.log(Math.random() * 10);
