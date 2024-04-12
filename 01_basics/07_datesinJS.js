8a5a50c64e3b39bea27c50c55468b21d9d77db93
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-12 13:39:53 +0000

    dates

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,18 @@
+//  Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 3);
+// let myCreatedDate = new Date(2023, 0, 3, 5, 3);
+let myCreatedDate = new Date("2024-01-23");
+
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
