b38f39fe5e6eb6b8e4502ecbe1eda0b59b524e53
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-12 16:46:28 +0000

    edit in dates

--- 01_basics/07_datesinJS.js
+++ 01_basics/07_datesinJS.js
@@ -13,6 +13,16 @@ let myCreatedDate = new Date("2024-01-23");
 // console.log(myCreatedDate.toDateString());
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now();
-console.log(myTimeStamp);
-console.log(myCreatedDate.getTime());
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weakday: "long"
+})
+
+console.log(newDate);
