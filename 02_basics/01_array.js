d727dcc187d0e4f56d5b63ef135121fb5f15dfe1
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-23 12:26:45 +0000

    array edited

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,3 +1,26 @@
 // arrays
 
 const myArr = [0, 1, 2, 3, 4, 5]
+
+// myArr.push(6,7);
+// myArr.pop();
+// myArr.shift();
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join();
+
+// console.log(newArr);
+// console.log(myArr);
+
+//  slice,  splice
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,4);
+console.log(myn1);
+
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,4);
+console.log("C",myArr);
+console.log(myn2);
