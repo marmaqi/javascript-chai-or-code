5af9037cda7f9b0b1e795ca5155b0269bb26f550
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-25 11:09:50 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+
+
+const isUserLoggedIn = true;
+if(isUserLoggedIn){
+
+
+}
+
+// <, >, <=, >=, ==, ====
+
+
+const balance = 1000;
+
+if(balance < 500 ){
+    console.log("Less than 500");
+}
+else if(balance < 750){
+    console.log("Less than 750");
+}
+else if(balance < 950){
+    console.log("Less than 950");
+}
+else{
+    console.log("Equal to 1000");
+}
