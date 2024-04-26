19ed810befde38736ac40b96f6fdd8c7f1d3208e
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-26 12:52:43 +0000

    truthy done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,15 +1,26 @@
 // const userEmail = "h@gmail.co"; 
-const userEmail = ""; 
+const userEmail = []; 
 
-if (userEmail) {    // this is truthy value where we don't use true or false in condition
-    console.log("User Exist");
-}
-else{
-    console.log("Don't have user");
-}
+// if (userEmail) {    // this is truthy value where we don't use true or false in condition
+//     console.log("User Exist");
+// }
+// else{
+//     console.log("Don't have user");
+// }
 
 // falsy values
 // false, 0, -0, BigInt 0n, "", undefined, null, NaN
 
 // truthy values
 // "0", 'false', " ", true, {}, [], function(){}
+
+// if(userEmail.length === 0)  // this if condition checks the array is empty or not
+// {
+//     console.log("Array is Empty");
+// }
+
+const emptyObject = {};
+if(Object.keys(emptyObject).length === 0)  // this if condition checks the array is empty or not
+{
+    console.log("OBJECT is Empty");
+}
