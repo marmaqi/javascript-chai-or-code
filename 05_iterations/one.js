300ec6b3f5f7573f739e9bd62d0e548c5a5c6a67
marmaqi <143022038+marmaqi@users.noreply.github.com>
2024-04-27 10:24:44 +0000

    ...

---
+++ 05_iterations/one.js
@@ -0,0 +1,20 @@
+// for
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+// for (let i = 1; i <= 10; i++) {
+//     for (let j =1; j <= 10; j++) {
+//         console.log(`${i} x ${j} = ${i*j}`);
+//     }
+//     console.log('');
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log(`The value is now ${element}`);
+//     }
+//     console.log(element);
+// }
